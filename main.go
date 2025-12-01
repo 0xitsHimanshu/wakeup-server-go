51cf6334d4fcf611358b54a199e45f0a5f36da30
Himanshu <70974599+0xitsHimanshu@users.noreply.github.com>
2025-12-01 15:35:03 +0530

    added db connection & models

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"wakeup-server-go/database"
+	"wakeup-server-go/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
 )
 
 func main(){
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	database.Connect()
+	if err := database.AutoMigrate(&models.User{}, &models.Log{}, &models.Task{}); err != nil {
+		log.Fatal("Error during database migration:", err)
+	}
+
 }
---
+++ models/models.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Email	string `json:"email" binding:"required,email"`
+	Tasks	[]Task `json:"tasks" gorm:"foreignKey:UserID"`
+}
+
+type Task struct {
+	gorm.Model
+	URL				string    `json:"url" binding:"required,url"`
+	IsActive		bool      `json:"isActive"`
+	NotifyDiscord	bool      `json:"notifyDiscord" gorm:"default:false"`
+	WebHook 		string    `json:"webHook" gorm:"default:null"`
+	UserID			uint      `json:"userId" binding:"required"`
+	Logs 			[]Log	  `json:"logs" gorm:"foreignKey:TaskID"`
+	FailCount		int       `json:"failCount" gorm:"default:0"`
+}
+
+type Log struct {
+	gorm.Model
+	TaskID 			uint      `json:"taskId" binding:"required" `
+	Timestamp		time.Time `json:"time"`
+	TimeTake 		int64     `json:"timeTake"`
+	LogResponse		string    `json:"logResponse"`
+	IsSuccess		bool      `json:"isSuccess"`
+	ResponseCode	int       `json:"responseCode"`
+}
+
